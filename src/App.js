b504a58e88a5634be31642f2831904ddb88ac6f3
Blue <casey.blue@capgemini.com>
2022-09-13 19:10:10 -0400

    no usestate

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Header from './components/Header';
 import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
     <div className="container">
       <Header/>
-      
-      <h1>test</h1>
+      <Carousel/>
       <Footer/>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ import NavBar from './NavBar'
 const Header = () => {
   return (
    <header className='Header'>
-    <img src='https://i.imgur.com/AdJh1wp.png' alt='Logo' className='logo'></img>
-    <img src='https://i.imgur.com/nBpjMln.png' alt='logo' className='logo right'></img>
+    <div><img src='https://i.imgur.com/AdJh1wp.png' alt='Logo' className='brand'></img></div>
+    <img src='https://i.imgur.com/nBpjMln.png' alt='logo' className='logo' style={{textAlign:'right'}}></img>
    </header>
   )
 }
