705e42dd8dbcb6e3d01272967e1db4e142ef5a2b
naimam <naima.mohamed@capgemini.com>
2022-09-14 23:08:55 -0400

    help

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,6 @@ import {
 
 import './NavBar.css';
 
-import { useAuthenticator } from "@aws-amplify/ui-react";
-import { Amplify } from "aws-amplify";
-
-import awsExports from "../aws-exports";
-Amplify.configure(awsExports);
 
 
 
